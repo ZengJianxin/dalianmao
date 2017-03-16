34b20d0454f378d82da497a9234eefb1517ff017
ZengJianxin <zengjx92@163.com>
2017-03-16 12:40:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup_kwargs = {
     'name': 'dalianmao',
-    'version': '0.02',
+    'version': '0.03',
     'description': 'A Web Crawling and Web Scraping microframework based on uvloop and aiohttp',
     'long_description': open('README.txt').read(),
     'packages': ['dalianmao',],
