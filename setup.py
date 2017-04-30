444d7077d150ce1a58fdc505bdab6a716b51ae7f
ZengJianxin <zengjx92@163.com>
2017-04-30 18:05:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from distutils.core import setup
 
 setup_kwargs = {
     'name': 'dalianmao',
-    'version': '0.07',
-    'description': 'A Web Crawling and Web Scraping microframework based on uvloop and aiohttp',
-    'long_description': open('README.md', encoding='utf-8').read(),
+    'version': '0.09',
+    'description': 'A Web Crawling and Web Scraping microframework based on aiohttp',
     'packages': ['dalianmao',],
     'platforms': 'any',
     'license': 'MIT',
@@ -19,26 +18,14 @@ setup_kwargs = {
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-}
-
-try:
-    normal_requirements = [
-        'uvloop>=0.5.3',
-        'aiohttp>=1.3.3',
-        'aiofiles>=0.3.0',
-        'beautifulsoup4>=4.2.0',
-        'motor>=1.1'
-    ]
-    setup_kwargs['install_requires'] = normal_requirements
-    setup(**setup_kwargs)
-except RuntimeError as exception:
-    windows_requirements = [
+    'requirements': [
         'aiohttp>=1.3.3'
         'aiofiles>=0.3.0',
         'beautifulsoup4>=4.2.0',
         'motor>=1.1'
     ]
-    setup_kwargs['install_requires'] = windows_requirements
-    setup(**setup_kwargs)
+}
+
+setup(**setup_kwargs)
 
 print('DaLianMao version {} successfully installed.'.format(setup_kwargs['version']))
