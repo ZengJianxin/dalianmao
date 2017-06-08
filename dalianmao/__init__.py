4f9f72067b9465e29b80370d6a628ebc2d05ddd9
ZengJianxin <zengjx92@163.com>
2017-06-08 16:35:11 +0800

    Create __init__.py

--- dalianmao/__init__.py
+++ dalianmao/__init__.py
@@ -4,5 +4,5 @@ from dalianmao.app import DaLianMao
 from dalianmao.options import Options
 import dalianmao.utils
 
-__version__ = '0.06'
+__version__ = '0.10'
 __all__ = [Executor, DaLianMao, Options]
